103339a461d9f38abcb4863dbc9c1ab83cebe847
wj <1724031211@qq.com>
2023-08-03 16:37:05 +0800

    会员端

---
+++ src/components/the-bottom.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer">
+        <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2023005697号-1</a>
+    </div>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import store from "@/store";
+import router from '@/router'
+
+export default defineComponent({
+    name: "the-bottom-view",
+    setup() {
+        let member = store.state.member;
+        const selectedKeys = ref([]);
+
+        watch(() => router.currentRoute.value.path, (newValue) => {
+            console.log('watch', newValue);
+            selectedKeys.value = [];
+            selectedKeys.value.push(newValue);
+        }, {immediate: true});
+        return {
+            member,
+            selectedKeys
+        };
+    },
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #f0f0f0;
+    padding: 10px;
+    text-align: center;
+    font-size: 12px;
+}
+</style>
---
+++ src/views/main.vue
@@ -0,0 +1,34 @@
+<template>
+    <a-layout id="components-layout-demo-top-side-2">
+        <the-header-view></the-header-view>
+        <a-layout>
+            <the-sider-view></the-sider-view>
+            <a-layout-content
+                    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+            >
+                <router-view></router-view>
+            </a-layout-content>
+        </a-layout>
+        <the-bottom-view></the-bottom-view>
+    </a-layout>
+</template>
+<script>
+import {defineComponent} from 'vue';
+import TheHeaderView from "@/components/the-header";
+import TheSiderView from "@/components/the-sider";
+import TheBottomView from "@/components/the-bottom";
+
+export default defineComponent({
+    components: {
+        TheSiderView,
+        TheHeaderView,
+        TheBottomView
+    },
+    setup() {
+        return {};
+    },
+});
+</script>
+<style>
+
+</style>
